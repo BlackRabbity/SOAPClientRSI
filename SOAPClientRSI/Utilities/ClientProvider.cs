5ddec869f5e819682137ea2b9356280fbc300e00
Piotr Go <piotrgo2000@gmail.com>
2024-05-14 09:49:42 +0200

    ip change

--- SOAPClientRSI/Utilities/ClientProvider.cs
+++ SOAPClientRSI/Utilities/ClientProvider.cs
@@ -16,10 +16,10 @@ namespace SOAPClientRSI.Utilities
             // MTOM message encoding
             BasicHttpBinding binding = new BasicHttpBinding();
             binding.MessageEncoding = WSMessageEncoding.Mtom;
-            binding.MaxReceivedMessageSize = 1000000;
+            binding.MaxReceivedMessageSize = 20000000;
 
 
-            EndpointAddress endpointAddress = new EndpointAddress("http://127.0.0.1:9999/ws/CinemaImpl?wsdl");
+            EndpointAddress endpointAddress = new EndpointAddress("http://192.168.200.9:9999/ws/CinemaImpl?wsdl");
 
             _client = new CinemaImplClient(binding, endpointAddress);
         }
